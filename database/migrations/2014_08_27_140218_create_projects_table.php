489613fea10c5bc8e4912fa102ac65338e012932
Abdelrahman Magdy EL Monshed <amgo123@gmail.com>
2023-12-28 15:43:04 +0200

    databse commit

--- database/migrations/2014_08_27_140218_create_projects_table.php
+++ database/migrations/2014_08_27_140218_create_projects_table.php
@@ -8,7 +8,7 @@ class CreateProjectsTable extends Migration
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->foreignId('form_id')->constrained('forms')->onDelete('cascade');
+            // $table->foreignId('form_id')->constrained('forms')->onDelete('cascade');
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
 			$table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
        });
