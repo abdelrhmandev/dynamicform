30936582f2ab4c4f46a222dd18ff440dda4e5c93
Abdelrahman Magdy EL Monshed <amgo123@gmail.com>
2024-01-17 17:13:43 +0200

    survey build up app 2

--- app/Http/Controllers/BuildingController.php
+++ app/Http/Controllers/BuildingController.php
@@ -1,13 +1,15 @@
 <?php
 namespace App\Http\Controllers;
 use Carbon\Carbon;
+use App\Models\Form;
+use App\Models\FormElement;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Models\Form as MainModel;
-use App\Models\FormElement;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\FormElementRequest as ModuleRequest;
+
 class BuildingController extends Controller
 {
     /**
@@ -33,10 +35,14 @@ class BuildingController extends Controller
   
     public function create()
     {
+
+        $form = Form::where('id','1')->first();
+
         if (view()->exists('buildings.answer')) {
             $compact = [
                 'listingRoute'  => route($this->ROUTE_PREFIX . '.index'),
                 'storeRoute'    => route($this->ROUTE_PREFIX . '.store'),
+                'form'          =>$form
             ];
             return view('buildings.answer', $compact);
         }
